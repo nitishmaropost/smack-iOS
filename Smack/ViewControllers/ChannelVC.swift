371c6a2a44ef4ce3237666e908976bd2f84f3521
Nitish <nitish@maropost.com>
2019-01-24 16:11:40 +0530

    Added side menu

---
+++ Smack/ViewControllers/ChannelVC.swift
@@ -0,0 +1,17 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by maropost on 24/01/19.
+//  Copyright © 2019 maropost. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+}
---
+++ Smack/ViewControllers/ChatVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by maropost on 24/01/19.
+//  Copyright © 2019 maropost. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+}
