f7300f38e6068217cdda35127472acdee9a64d01
Nitish <nitish@maropost.com>
2019-01-24 18:24:15 +0530

    added login screen

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by maropost on 24/01/19.
+//  Copyright © 2019 maropost. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
---
+++ Smack/ViewControllers/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by maropost on 24/01/19.
+//  Copyright © 2019 maropost. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func closedPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
